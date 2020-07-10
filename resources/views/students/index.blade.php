90ac1309345d649fa8ba5d80dca32659598346e7
Momo Ramadori <momoramadori8@gmail.com>
2020-07-10 18:22:09 +0200

    bootstrap-confirmation try failed

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -31,7 +31,7 @@
                             <td class='text-right'>
                             <a class='btn btn-info bt-small' href="{{ route('students.show', ['student' => $studente->id]) }}"> Dettagli </a>
                             <a class='btn btn-warning bt-small' href="{{ route('students.edit', ['student' => $studente->id]) }}"> Modifica </a>
-                            <form class='d-inline-block' action="{{ route('students.destroy', ['student' => $studente->id]) }}" method="POST" data-toggle="confirmation">
+                            <form class='d-inline-block' action="{{ route('students.destroy', ['student' => $studente->id]) }}" method="POST" onclick="return confirm('Are you sure?')" >
                               @method('DELETE')
                               @csrf 
                               <input type='submit' class='btn btn-danger bt-small' value='Elimina'>
